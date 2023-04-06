ae4610235d098b6147655568acc592c476485a0d
Yevhenii Sukach <sukachy@digicode.ua>
2023-04-06 17:16:29 +0300

    page 404 is done

--- functions.php
+++ functions.php
@@ -24,6 +24,7 @@ function my_theme_enqueue_files()
   wp_enqueue_style('front-page', get_template_directory_uri() . '/css/front-page.css');
   wp_enqueue_style('our-services', get_template_directory_uri() . '/css/our-services.css');
   wp_enqueue_style('about-us', get_template_directory_uri() . '/css/about-us.css');
+  wp_enqueue_style('404', get_template_directory_uri() . '/css/404.css');
 
   wp_enqueue_style('template-page-contact-us', get_template_directory_uri() . '/css/template-page-contact-us.css');
   wp_enqueue_style('template-page-team', get_template_directory_uri() . '/css/template-page-team.css');
